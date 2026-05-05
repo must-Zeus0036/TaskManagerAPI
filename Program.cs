9559e7d2347d1a11d5444b469817d3f85fea4292
must-Zeus0036 <mustafa.al-bayati0036@stud.hkr.se>
2026-05-05 10:35:40 +0200

    added Swagger

--- Program.cs
+++ Program.cs
@@ -1,39 +1,47 @@
 using System.Collections.Generic;
 using System.Linq;
+using TaskManagerAPI;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-var tasks = new List<TaskManagerAPI.TaskItem>();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+var tasks = new List<TaskItem>();
 
 app.MapGet("/tasks", () => tasks);
 
-app.MapPost("/tasks", (TaskManagerAPI.TaskItem task) =>
+app.MapPost("/tasks", (TaskItem task) =>
 {
     task.Id = tasks.Count + 1;
     tasks.Add(task);
-    return task;
+    return Results.Ok(task);
 });
 
-app.MapPut("/tasks/{id}", (int id, TaskManagerAPI.TaskItem updatedTask) =>
+app.MapPut("/tasks/{id}", (int id, TaskItem updatedTask) =>
 {
     var task = tasks.FirstOrDefault(t => t.Id == id);
-    if (task == null)
-    {
-        return Results.NotFound();
-    }
 
+    if (task == null)
+        return Results.NotFound();
+
+    task.Title = updatedTask.Title;
     task.IsDone = updatedTask.IsDone;
+
     return Results.Ok(task);
 });
 
 app.MapDelete("/tasks/{id}", (int id) =>
 {
     var task = tasks.FirstOrDefault(t => t.Id == id);
+
     if (task == null)
-    {
         return Results.NotFound();
-    }
 
     tasks.Remove(task);
     return Results.Ok();
